ea32d9891292f58d01ba444f567289e8b4da974f
MatteoRigolin <72465769+MatteoRigolin@users.noreply.github.com>
2021-10-29 08:33:12 +0200

    Add files via upload

---
+++ Fantacalcio/Fantacalcio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantacalcio
+{
+    class Fantacalcio
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Giocatore
+    {
+        string nome;
+        List<string> rosa;
+        List<string> formazione;
+        int punteggio;
+        public Giocatore()
+        {
+            this.nome = nome;
+        }
+    }
+}
