fb24969ffa600278e466a788a618c1c6c2543ee0
MatteoRigolin <72465769+MatteoRigolin@users.noreply.github.com>
2021-10-29 09:24:02 +0200

    Add files via upload

--- Fantacalcio/Fantacalcio/Program.cs
+++ Fantacalcio/Fantacalcio/Program.cs
@@ -8,19 +8,81 @@ namespace Fantacalcio
 {
     class Fantacalcio
     {
+        protected void aggiungiGiocatori()
+        {
+
+        }
+        protected void asta()
+        {
+
+        }
+        protected void creaGironi()
+        {
+
+        }
+        protected void partita()
+        {
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Benvenuto nel simulatore di fantacalcio");
+            int numeroGiocatori;
+            
+            Console.WriteLine("Scegli il numero di giocatori:\n ");
+            Console.WriteLine("Premi 2 per la modalità a 2 giocatori");
         }
     }
     class Giocatore
     {
         string nome;
-        List<string> rosa;
-        List<string> formazione;
+        List<Calciatore> rosa;
+        List<Calciatore> formazione;
+        int puntiSquadra;
         int punteggio;
         public Giocatore()
         {
-            this.nome = nome;
+            
+        }
+        protected string setNomeGiocatore()
+        {
+            return nome;
+        }
+        protected void setRosa()
+        {
+
+        }
+        protected void setFormazione()
+        {
+
+        }
+        protected int setBonus()
+        {
+            return puntiSquadra;
+        }
+        protected int setMalus()
+        {
+            return puntiSquadra;
+        }
+        protected int setPunteggioGiocatore()
+        {
+            return punteggio;
+        }
+    }
+    class Calciatore
+    {
+        string nome;
+        string ruolo;
+        string squadra;
+        int prezzo;
+        int voto;
+        public Calciatore()
+        {
+
+        }
+        protected int setVotoGiocatore()
+        {
+            return voto;
         }
     }
 }
