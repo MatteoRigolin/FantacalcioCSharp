77b8067bb3529a25cc21371f5c28ae4fc1fb7b37
MatteoRigolin <72465769+MatteoRigolin@users.noreply.github.com>
2021-11-04 10:17:36 +0100

    Creazione Main e Metodi

--- Fantacalcio/Fantacalcio/Program.cs
+++ Fantacalcio/Fantacalcio/Program.cs
@@ -8,63 +8,66 @@ namespace Fantacalcio
 {
     class Fantacalcio
     {
-        protected void aggiungiGiocatori()
+        List<Giocatore> giocatori = new List<Giocatore>();
+        public Fantacalcio(List<Giocatore> giocatori)
         {
-
+            this.giocatori = giocatori;
         }
-        protected void asta()
-        {
-
-        }
-        protected void creaGironi()
-        {
-
-        }
-        protected void partita()
-        {
-
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Benvenuto nel simulatore di fantacalcio");
-            int numeroGiocatori;
+        //public List<Giocatore> setGiocatori()
+        //{
             
-            Console.WriteLine("Scegli il numero di giocatori:\n ");
-            Console.WriteLine("Premi 2 per la modalità a 2 giocatori");
+        //    return giocatori;
+        //}
+        public void asta()
+        {
+            
         }
+        public void creaGironi()
+        {
+
+        }
+        public void partita()
+        {
+
+        }        
     }
     class Giocatore
     {
         string nome;
+        int fantamilioni;
         List<Calciatore> rosa;
         List<Calciatore> formazione;
         int puntiSquadra;
         int punteggio;
-        public Giocatore()
+        public Giocatore(string nome, int fantamilioni, int puntiSquadra, int punteggio)
         {
-            
+            this.nome = nome;
+            this.fantamilioni = fantamilioni;
+            this.puntiSquadra = puntiSquadra;
+            this.punteggio = punteggio;
         }
-        protected string setNomeGiocatore()
+        public string setNomeGiocatore()
         {
+            nome = Console.ReadLine();
             return nome;
         }
-        protected void setRosa()
+        public void setRosa()
         {
 
         }
-        protected void setFormazione()
+        public void setFormazione()
         {
 
         }
-        protected int setBonus()
+        public int setBonus()
         {
             return puntiSquadra;
         }
-        protected int setMalus()
+        public int setMalus()
         {
             return puntiSquadra;
         }
-        protected int setPunteggioGiocatore()
+        public int setPunteggioGiocatore()
         {
             return punteggio;
         }
@@ -76,13 +79,42 @@ namespace Fantacalcio
         string squadra;
         int prezzo;
         int voto;
-        public Calciatore()
+        public Calciatore(string nome, string ruolo, string squadra, int prezzo, int voto)
         {
-
+            this.nome = nome;
+            this.ruolo = ruolo;
+            this.squadra = squadra;
+            this.prezzo = prezzo;
+            this.voto = voto;
         }
-        protected int setVotoGiocatore()
+        public int setVotoGiocatore()
         {
             return voto;
+        }
+    }
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Benvenuto nel simulatore di fantacalcio");
+            Console.WriteLine("Puoi scegliere tra tre modalità:\n2 giocatori\n4 giocatori\n8 giocatori");
+            Console.WriteLine("Inserisci il numero di giocatori:\n ");
+            int numeroGiocatori;
+            numeroGiocatori = Convert.ToInt32(Console.ReadLine());
+            while (numeroGiocatori == 2 || numeroGiocatori == 4 || numeroGiocatori == 8)
+            {
+                Console.WriteLine("Devi inserire solo 2, 4 o 8");
+                numeroGiocatori = Convert.ToInt32(Console.ReadLine());
+            }
+            List<Giocatore> giocatori = new List<Giocatore>();
+            Fantacalcio fantacalcio = new Fantacalcio(giocatori);
+            for (int i = 0; i < numeroGiocatori; i++)
+            {
+                Console.WriteLine("Inserisci il nome del giocatore {0}", i + 1);
+                giocatori.Add(new Giocatore("", 500, 0, 0));
+
+            }
+            Console.ReadKey();
         }
     }
 }
